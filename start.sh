2917afd030688c2213ac89c7fe0ec5b35a65631a
victoria makena <makenavictoria1@gmail.com>
2022-05-15 01:43:40 +0300

    add credentials

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='tori-blogs'
+export DATABASE_URI='postgresql+psycopg2://elvis:moraaelvis@localhost/blog'
+
+export MAIL_USERNAME='makenavictoria1@gmail.com'
+export MAIL_PASSWORD='golden5626era.?!'
+python3.9 manage.py server
