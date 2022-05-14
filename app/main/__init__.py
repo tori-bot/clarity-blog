1fc58aad87851fd700ba7c76308f99d0152af181
victoria makena <makenavictoria1@gmail.com>
2022-05-15 02:16:02 +0300

    add main app factory

---
+++ app/main/__init__.py
@@ -0,0 +1,19 @@
+from ensurepip import bootstrap
+from flask import Blueprint,Flask
+from config import config_options
+
+main=Blueprint('main',__name__)
+
+from . import views,errors
+
+def create_app(config_name):
+    app=Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+
+    from ..main import main as main_blueprint
+    app.register_blueprint(main.blueprint)
+
+    return app
