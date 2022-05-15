1b544f075c4253170513ee4212ca3ce26929e828
victoria makena <makenavictoria1@gmail.com>
2022-05-15 03:02:25 +0300

    add auth blueprint and forms

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from flask import render_template,redirect,url_for,abort
 from . import main
+from flask_login import current_user
 
 @main.route('/')
 def index():
