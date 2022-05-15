013535cfb04890d9e950cbc2250f062ffcae19be
victoria makena <makenavictoria1@gmail.com>
2022-05-15 13:49:33 +0300

    add email configuration

---
+++ app/email.py
@@ -0,0 +1,9 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+def mail_message(subject,template,to,**kwargs):
+    sender_email = 'makenavictoria1@gmail.com'
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
