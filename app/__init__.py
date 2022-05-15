3e1eb5e08c53206ae9d910bb814c07f2c183e55a
victoria makena <makenavictoria1@gmail.com>
2022-05-16 02:02:24 +0300

    add profile photo configuration in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from werkzeug import secure_filename,FileStorage
 
 
 login_manager=LoginManager()
@@ -14,17 +16,21 @@ login_manager.login_view='auth.login'
 db=SQLAlchemy()
 bootstrap=Bootstrap()
 mail=Mail()
-
+photos=UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app=Flask(__name__)
 
     app.config.from_object(config_options[config_name])
 
+    configure_uploads(app,photos)
+    
+    
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
