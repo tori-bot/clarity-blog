2acea89208ead592aaaef34c8cc1fccc526a9258
victoria makena <makenavictoria1@gmail.com>
2022-05-15 13:18:14 +0300

    add auth views and forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from sys import prefix
 from flask import Flask
 from config import config_options
 
@@ -8,5 +9,8 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,prefix='auth')
 
     return app
--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
-from app import create_app
+from app import create_app,db
+from app.models import User
 from flask_script import Server,Manager
+from flask_migrate import Migrate,MigrateCommand
 
 app=create_app('development')
 
 manager=Manager(app)
 manager.add_command('server',Server)
+
+migrate=Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__=='__main__':
     manager.run()
